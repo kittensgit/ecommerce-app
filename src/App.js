d41b4b4e18d18c03fcc7c4f5880a951de843c703
Nika <nikaonime@gmail.com>
2023-07-29 19:23:26 +0300

    react-router-dom, Header, font awesome cdn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import data from './components/back/data/Data';
+import Header from './components/front/Header/Header';
 
 function App() {
-    return <div className="App"></div>;
+    const { productItems } = data;
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route path="/" element={<div>hello</div>} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
