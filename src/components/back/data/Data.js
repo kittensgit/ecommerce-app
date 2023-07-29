557e0dd6e5f066d8ea6d01d7fb6fea4b8a0dc5b4
Nika <nikaonime@gmail.com>
2023-07-29 16:29:56 +0300

    init project, data(productItems)

---
+++ src/components/back/data/Data.js
@@ -0,0 +1,60 @@
+const data = {
+    productItems: [
+        {
+            id: '1',
+            name: 'Beats HeadPhone',
+            price: 999,
+            image: '',
+        },
+        {
+            id: '2',
+            name: 'Sol HeadPhone',
+            price: 899,
+            image: '',
+        },
+        {
+            id: '3',
+            name: 'Sennheiser',
+            price: 799,
+            image: '',
+        },
+        {
+            id: '4',
+            name: 'IphoneX',
+            price: 1999,
+            image: '',
+        },
+        {
+            id: '5',
+            name: 'Samsung',
+            price: 1899,
+            image: '',
+        },
+        {
+            id: '6',
+            name: 'OnePLus',
+            price: 1299,
+            image: '',
+        },
+        {
+            id: '7',
+            name: 'Macbook Pro',
+            price: 2999,
+            image: '',
+        },
+        {
+            id: '8',
+            name: 'Acer Laptop',
+            price: 1099,
+            image: '',
+        },
+        {
+            id: '9',
+            name: 'Dell Laptop',
+            price: 1399,
+            image: '',
+        },
+    ],
+};
+
+export default data;
