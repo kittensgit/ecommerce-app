9132075446f5d964cb1408a4c51a76336bd3d64b
Nika <nikaonime@gmail.com>
2023-07-30 14:11:18 +0300

    Products comp(useState,useEffect, fetch, b styles)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
+import Products from './Products';
 
 const Home = () => {
     return (
         <div className="hero">
-            <div class="card bg-dark text-white border-0">
+            <div className="card bg-dark text-white border-0">
                 <img
                     src="/assets/bg.jpg"
-                    class="card-img"
+                    className="card-img"
                     alt="bg"
                     height="550px"
                 />
-                <div class="card-img-overlay d-flex flex-column justify-content-center">
+                <div className="card-img-overlay d-flex flex-column justify-content-center">
                     <div className="container">
-                        <h5 class="card-title display-3 fw-bolder mb-0">
+                        <h5 className="card-title display-3 fw-bolder mb-0">
                             NEW SEASON ARRIVALS
                         </h5>
-                        <p class="card-text lead fs-2">
+                        <p className="card-text lead fs-2">
                             CHECK OUT ALL THE TRENDS
                         </p>
                     </div>
                 </div>
             </div>
+
+            <Products />
         </div>
     );
 };
