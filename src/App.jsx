9f2025fafd91fb6976623b299a3030510bfcd58e
Nika <nikaonime@gmail.com>
2023-07-30 11:37:31 +0300

    clear project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import data from './components/back/data/Data';
-import Header from './components/front/Header/Header';
-import Products from './components/front/Products/Products';
 
 function App() {
-    const { productItems } = data;
-    return (
-        <div>
-            <Header />
-            <Routes>
-                <Route
-                    path="/"
-                    element={<Products productItems={productItems} />}
-                />
-            </Routes>
-        </div>
-    );
+    return <div></div>;
 }
 
 export default App;
